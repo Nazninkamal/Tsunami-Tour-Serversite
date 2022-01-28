04cc52bf9411f8b7d11174fe3108f2cf25858f55
NazninKamal <najninkamalrimi@gmail.com>
2022-01-28 22:03:55 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,101 +1,112 @@
-const express = require('express');
+const express = require('express')
 const { MongoClient } = require('mongodb');
-const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
-const fileUpload = require('express-fileupload')
+
+const cors = require('cors');
 require('dotenv').config();
-
-const app = express();
+const app = express()
 const port = process.env.PORT || 5000;
+;
 
-//middleaware
+//middelware
 app.use(cors());
-app.use(express.json());
-app.use(fileUpload())
-
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.orb6q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
 async function run() {
     try {
-        await client.connect();
+      await client.connect();
+      //console.log('connected to the database')
+      const database = client.db("tsunami-tour");
+      const blogsCollection = database.collection("blogs");
+      const experienceCollection = database.collection("experience");
+      const reviewCollection = database.collection("review");
+      
+      //get api
+      app.get('/blogs', async(req, res)=>{
+        const cursor = blogsCollection.find({});
+        const blogs = await cursor.toArray();
+        res.send(blogs);
+      })
+      app.get('/experience', async(req, res)=>{
+        const cursor = experienceCollection.find({});
+        const experience = await cursor.toArray();
+        res.send(experience);
+      })
+      app.get('/review', async(req, res)=>{
+        const cursor = reviewCollection.find({});
+        const review = await cursor.toArray();
+        res.send(review);
+      })
 
-        const database = client.db("tsunami-tour");
-        const blogCollection = database.collection("blogs");
-        const usersCollection = database.collection("users");
+      //get single service
+      app.get('/blogs/:id', async(req, res) =>{
+        const id = req.params.id;
+        console.log('getting specific service', id)
+        const query = {_id: ObjectId(id)};
+        const service = await blogsCollection.findOne(query);
+        res.json(service);
+      })
+
+      //Post api
+      app.post('/blogs', async(req, res)=>{
+        const service = req.body;
+         console.log('hit the post api', service);
+
+         const result = await blogsCollection.insertOne(service);
+         console.log(result);
+         res.json(result)
+      
+        }) ;
+
+        app.post('/experience', async(req, res)=>{
+          const user = req.body;
+           console.log('hit the post api', user);
+  
+           const result = await experienceCollection.insertOne(user);
+           console.log(result);
+           res.json(result)
+        
+          }) ;
+          app.post('/review', async(req, res)=>{
+            const review = req.body;
+             console.log('hit the post api', review);
+    
+             const result = await reviewCollection.insertOne(review);
+             console.log(result);
+             res.json(result)
+          
+            }) ;
 
 
-        //POST API- New Blog
-        app.post('/blogs', async (req, res) => {
-            const name = req.body.name;
-            const address = req.body.address;
-            const location = req.body.location;
-            const expense = req.body.expense;
-            const email = req.body.email;
-            const date = req.body.date;
-            const time = req.body.time;
-            const status = req.body.status;
-            const rating = req.body.rating;
-            const pic = req.files.image;
+        // delete api
+        app.delete('/experience/:id',async(req, res)=>{
+          const id = req.params.id;
+          const query = {_id:ObjectId}
+          const result = await experienceCollection.deleteOne(query);
+          res.json(result);
+        })
 
-            const picData = pic.data;
-            const encodedPic = picData.toString('base64');
-            const imgBuffer = Buffer.from(encodedPic, 'base64');
-            const brand = {
-                name,
-                address,
-                location,
-                expense,
-                email,
-                date,
-                time,
-                rating,
-                status,
-                image: imgBuffer
-            };
-            const result = await blogCollection.insertOne(brand);
-            console.log(result);
-            res.json(result);
-        });
-
-
-        //save users
-        app.post('/users', async (req, res) => {
-            const user = await usersCollection.insertOne(req.body);
-            console.log(user);
-            res.json(user);
-        });
-
-        // UPDATE API - users
-
-        app.put('/users', async (req, res) => {
-            const user = req.body;
-            const filter = { email: user.email };
-            const options = { upsert: true };
-            const updateUser = { $set: user }
-            const result = await usersCollection.updateOne(filter, updateUser, options);
-            res.json(result);
-
-        });
-
-        console.log('database connected successfully');
-
-    } finally {
-        //await client.close();
+    } 
+    
+    finally {
+      //await client.close();
     }
-}
+  }
 
-run().catch(console.dir);
+  run().catch(console.dir);
 
+      
 
 app.get('/', (req, res) => {
-    res.send(' server is running');
+  res.send('Tsunami Tour');
 });
 
 app.listen(port, () => {
-    console.log('server running at port', port);
-});
+  console.log('Running tsunami tour on port',port)
+})
